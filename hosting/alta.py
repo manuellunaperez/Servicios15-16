50a3ce497718376278da8d6cf9f4da35e7f6ae12
ManuelLunaPerez <manuellunaperez@gmail.com>
2016-02-26 20:00:46 +0100

    Corrigiendo errores0

--- hosting/alta.py
+++ hosting/alta.py
@@ -1,1 +1,18 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import string
+from random import choice
+import crypt
+	
+usuario = sys.argv[1]
+dominio = sys.argv[2]
 
+plantilla_usuario=open('plantillas/usuario.ldif','r')
+contenido_plantilla= plantilla_usuario.read()
+plantilla_usuario.close()
+crearusuario= open('usuario.ldif','w')
+contenido_plantilla = contenido_plantilla.replace('[[nombreusuario]]', usuario)
+contenido_plantilla = contenido_plantilla.replace('[[nombredominio]]', dominio)
+crearusuario.write(contenido_plantilla)
+crearusuario.close()
