0549fbbbf588f124d8ba2bd006b95383956ab753
ManuelLunaPerez <manuellunaperez@gmail.com>
2016-03-01 16:36:32 +0100

    Añadiendo script de baja

--- hosting/alta.py
+++ hosting/alta.py
@@ -62,14 +62,14 @@ else:
 
 #Creamos el directorio personal del usuario y le asignamos los permisos correspondientes
 	print "Creando directorio personal..."
-	os.system("mkdir /home/tuhosting.com/"+usuario+" ; cp -r /etc/skel/.* /home/tuhosting.com/"+usuario+"/ ; chown -R "+uidmax+":2001 /home/tuhosting.com/"+usuario+"")
+	os.system("mkdir /home/tuhosting.com/"+usuario+" ; cp /etc/skel/.* /home/tuhosting.com/"+usuario+"/ > /dev/null; chown -R "+uidmax+":2001 /home/tuhosting.com/"+usuario+"")
 
 #Asignamos la cuota de 100 MB al usuario
 	print "Asignando cuota de espacio"
 	os.system("quotatool -u "+usuario+" -bq 90M -l '100 Mb' /home/tuhosting.com")
 	
 #Introducimos la plantilla web en el directorio del usuario:
-	os.system("cp -r  plantillas/cyanspark/* /home/tuhosting.com/"+usuario+"/")
+	os.system("cp -dPr plantillas/cyanspark/* /home/tuhosting.com/"+usuario+"/")
 	plantilla_web=open("/home/tuhosting.com/"+usuario+"/index.html","r")
 	contenido_plantillaweb= plantilla_web.read()
 	plantilla_web.close()
@@ -81,10 +81,10 @@ else:
 #Creamos el nuevo virtual host utilizando la plantilla.
 	print "Añadiendo el nuevo dominio al servidor web"
 	os.system ("cd /etc/apache2/sites-available")
-	os.system("touch "+dominio+".conf")
 	plantilla_vhost=open('plantillas/virtualhost.conf','r')
 	contenido_plantillavhost= plantilla_vhost.read()
 	plantilla_vhost.close()
+	os.system("touch "+dominio+".conf")
 	crearvhost = open("/etc/apache2/sites-available/"+dominio+".conf","w")
 	contenido_plantillavhost = contenido_plantillavhost.replace('[[nombreusuario]]', usuario)
 	contenido_plantillavhost = contenido_plantillavhost.replace('[[nombredominio]]', dominio)
@@ -95,7 +95,7 @@ else:
 #Creamos el nuevo usuario virtual para la gestión del ftp, lo almacenamos en uan base de datos. 
 	bd = MySQLdb.connect("localhost","root","root","netftp" )
 	cursor = bd.cursor()
-	cursor.execute("INSERT INTO `ftpuser` (`id`, `userid`, `passwd`, `uid`, `gid`, `homedir`, `shell`, `count`, `accessed`, `modified`) VALUES ('', '"+usuario+"_ftp', ENCRYPT('"+genpassftp+"'), 2005, 2005, 'home/tuhosting.com/"+usuario+"/', '/sbin/nologin', 0, '', ''); ")
+	cursor.execute("INSERT INTO `ftpuser` (`id`, `userid`, `passwd`, `uid`, `gid`, `homedir`, `shell`, `count`, `accessed`, `modified`) VALUES ('', '"+usuario+"_ftp', ENCRYPT('"+genpassftp+"'), 2005, 2005, '/home/tuhosting.com/"+usuario+"/', '/sbin/nologin', 0, '', ''); ")
 	bd.commit()
 	bd.close()
 	print("El usuario y contraseña para la administración ftp son:")
---
+++ hosting/baja.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import string
+from random import choice
+import crypt
+import commands
+import MySQLdb
+
+dominio = sys.argv[1]
+
+
+#Buscamos el usuario y nombre de dominio
+busquedadominio = commands.getoutput("ldapsearch -Y EXTERNAL -H ldapi:/// -Q -D 'cn=admin,dc=tuhosting,dc=com' -b 'dc=tuhosting,dc=com' 'o="+dominio+"' | grep numEntries:")
+
+if  busquedadominio != "#numEntries: 1":
+	print "El dominio no se encuentra"
+	exit()
+else:
+	print "Borrando dominio"
+	usuario = commands.getoutput("ldapsearch -Y EXTERNAL -H ldapi:/// -Q -D 'cn=admin,dc=tuhosting,dc=com' -b 'dc=tuhosting,dc=com' 'o="+dominio+"' | grep description: | sed -e 's,description: ,,'")
+#Borramos usuario y dominio de ldap 	
+	ldapdelete -x -D "cn=admin,dc=tuhosting,dc=com" -wroot "o="+dominio+",dc=tuhosting,dc=com" ;
+	ldapdelete -x -D "cn=admin,dc=tuhosting,dc=com" -wroot "uid="+usuario+",ou=People,dc=tuhosting,dc=com"
+
+#Borramos el directorio personal del usuario
+	print "Borrando directorio personal..."
+	os.system("rm -fr /home/tuhosting.com/"+usuario+"")
+	
+
+#Borramos el nuevo virtual host.
+	print "Borrando virtual host"
+	os.system("a2dissite /etc/apache2/sites-enabled/"+dominio+".conf > /dev/null")
+	os.system("rm /etc/apache2/sites-available/"+dominio+".conf")
+	
+#Borramos elusuario virtual para la gestión del ftp, lo almacenamos en uan base de datos. 
+	bd = MySQLdb.connect("localhost","root","root","netftp" )
+	cursor = bd.cursor()
+	cursor.execute("DELETE FROM ftpuser WHERE userid='"+usuario+"_ftp';")
+	bd.commit()
+	bd.close()
+	print("Usuario ftp borrado")	
+	
+#Creamos un nuevo usuario y una nueva base de datos para el usuario.
+	acciones = ["drop user 'my"+usuario+"'@'localhost'","drop database "+usuario+""]
+	for i in acciones:
+		os.system('mysql -u root -proot -e "'+i+'"')
+		
+	print("El usuario para la administración de la base de datos ha sido borrado")
+	
+#Definimos el nombre de dominio para la resolución dns.
+	numdominio = command.getoutput('cat /etc/bind/named.conf.local |grep -n  zone "'+dominio+'" | sed -e :zone "'+dominio+'" { ')
+	lineas = int(numdominio)+3
+	print "Borrando zona de resolución directa..."
+	os.syemtem("sed -i '"+int(numdominio)+","+lineas+"d' /etc/bind/named.conf.local")
+	os.system('rm /var/cache/bind/db.'+dominio+'')
+
+os.system("service apache2 reload")
+os.system("service proftpd reload")
+os.system("service bind9 reload")
